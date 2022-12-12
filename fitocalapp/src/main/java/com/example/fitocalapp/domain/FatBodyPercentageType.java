154ce4f6bf3a26af5c06fbc8cb0e11d62be88c17
furkanertan <furkanertan23@gmail.com>
2022-12-12 22:08:44 +0100

    Updating enum for fat body percentage

--- fitocalapp/src/main/java/com/example/fitocalapp/domain/FatBodyPercentageType.java
+++ fitocalapp/src/main/java/com/example/fitocalapp/domain/FatBodyPercentageType.java
@@ -10,12 +10,12 @@ public enum FatBodyPercentageType {
     ESSENTIAL_FAT_FEMALE("F","Essential Fat","10-13%"),
     ATHLETES_MALE("M","Athletes","6-13%"),
     ATHLETES_FEMALE("F","Athletes","14-20%"),
-    FIT_MALE("Fit","","14-17%"),
-    FIT_FEMALE("Fit","","21-24%"),
-    AVERAGE_MALE("Average","","18-24%"),
-    AVERAGE_FEMALE("Average","","25-31%"),
-    OBESE_MALE("Obese","","25% and above"),
-    OBESE_FEMALE("Obese","","32% and above");
+    FIT_MALE("M","Fit","14-17%"),
+    FIT_FEMALE("F","Fit","21-24%"),
+    AVERAGE_MALE("M","Average","18-24%"),
+    AVERAGE_FEMALE("F","Average","25-31%"),
+    OBESE_MALE("M","Obese","25% and above"),
+    OBESE_FEMALE("F","Obese","32% and above");
 
     private final String gender;
     private final String fatBodyPercentageType;
